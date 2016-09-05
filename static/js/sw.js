6ef179691558a982c333c96f91dc203ba75b98de
OmerHerera <omer.herera@gmail.com>
2016-09-05 17:24:50 +0300

    adding pages

---
+++ static/js/sw.js
@@ -0,0 +1,36 @@
+// Use a cacheName for cache versioning
+var cacheName = 'v1:static';
+
+// During the installation phase, you'll usually want to cache static assets.
+self.addEventListener('install', function(e) {
+    console.log('install is running ...')
+    // Once the service worker is installed, go ahead and fetch the resources to make this work offline.
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './../static/css/main.c8a935cd.css',
+                './../static/js/main.985f183d.js',
+                './../static/media/roboto.e5d1ccfb.woff',
+                './../static/media/logo.5d5d9eef.svg'
+            ]).then(function() {
+                self.skipWaiting();
+            });
+        })
+    );
+});
+
+// when the browser fetches a URL…
+self.addEventListener('fetch', function(event) {
+    // … either respond with the cached object or go ahead and fetch the actual URL
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
+});
